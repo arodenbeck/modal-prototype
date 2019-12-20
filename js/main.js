53168ac7b2dbbb325e90810c65beb2782ef51155
Adam Rodenbeck <arodenbeck@salesforce.com>
2019-12-19 16:13:49 -0800

    Removed unused functions and console logs.

--- js/main.js
+++ js/main.js
@@ -4,20 +4,7 @@ const ariaAnnouncement = document.getElementById('event-announcement')
 const modalButton = document.getElementById('jsModalButton')
 const modalCloseButton = document.getElementById('jsModalClose')
 const modalOverlay = document.querySelector('.modal-overlay')
-const wavingHand = document.querySelector('.wave-hand')
 const selectElement = document.getElementById('selectable-options')
-
-const wave = hand => {
-  const tl = new TimelineMax({})
-  // Sets transform origin
-  tl.set(hand, { transformOrigin: 'bottom center' })
-  tl.from(hand, 0.5, { scale: 0.25, opacity: 0, ease: Back.easeOut.config(1.5) })
-  tl.to(hand, 0.2, { rotation: 15 })
-  tl.to(hand, 0.2, { rotation: -15 })
-  tl.to(hand, 0.2, { rotation: 15 })
-  tl.to(hand, 0.2, { rotation: -15 })
-  tl.to(hand, 0.2, { rotation: 0 })
-}
 
 const closeModal = _ => {
   document.body.classList.remove('modal-is-open')
@@ -31,8 +18,6 @@ const openModal = _ => {
   modalIsOpen = true
   document.body.classList.add('modal-is-open')
   modalOverlay.setAttribute('aria-modal', 'true')
-  console.log("Does close button exist: " + modalCloseButton)
-  //wave(wavingHand)
   modalCloseButton.focus()
 }
 
